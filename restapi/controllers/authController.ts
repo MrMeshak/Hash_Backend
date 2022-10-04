1f4c9695cace624f48a33c198f58356a0cd1d299
MrMeshak <94204153+MrMeshak@users.noreply.github.com>
2022-10-04 19:01:16 +1100

    Configured Cookie Settings

--- restapi/controllers/authController.ts
+++ restapi/controllers/authController.ts
@@ -42,8 +42,8 @@ export const login = async (req: Request, res: Response) => {
     const user = await loginUser(email, password);
     console.log(user);
     const token = createToken(user.id);
-    res.cookie('authToken', token, { maxAge: 370000, httpOnly: true, sameSite: 'strict', secure: true, domain: 'hashbackend-production.up.railway.app' });
-    res.cookie('authToken', token, { maxAge: 370000, httpOnly: true, sameSite: 'strict', secure: true, domain: 'hash.up.railway.app' });
+    res.cookie('authToken', token, { maxAge: 370000, httpOnly: true, sameSite: 'strict', secure: true, domain: '.up.railway.app' });
+    res.cookie('authToken', token, { maxAge: 370000, httpOnly: true, sameSite: 'strict', secure: true, domain: '.up.railway.app' });
     res.status(200).json({
       user: removeSensitiveUserData(user)
     });
@@ -54,7 +54,7 @@ export const login = async (req: Request, res: Response) => {
 };
 
 export const logout = async (req: Request, res: Response) => {
-  res.cookie('authToken', '', { maxAge: 370000, httpOnly: true, sameSite: 'strict', secure: true, domain: 'hashbackend-production.up.railway.app' });
-  res.cookie('authToken', '', { maxAge: 370000, httpOnly: true, sameSite: 'strict', secure: true, domain: 'hash.up.railway.app' });
+  res.cookie('authToken', '', { maxAge: 370000, httpOnly: true, sameSite: 'strict', secure: true, domain: '.up.railway.app' });
+  res.cookie('authToken', '', { maxAge: 370000, httpOnly: true, sameSite: 'strict', secure: true, domain: '.up.railway.app' });
   res.status(200).json({ message: 'successfully logged out' });
 };
