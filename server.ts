b093b38b0de9741f3b59114437529e3e3891f5fe
Meshak Bain <94204153+MrMeshak@users.noreply.github.com>
2023-01-19 03:35:06 +1100

    Update server.ts

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ import cookieParser from 'cookie-parser';
 const app = express();
 
 //middleware
-app.use(cors({ origin: 'up.railway.app', credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use((req, res, next) => {
